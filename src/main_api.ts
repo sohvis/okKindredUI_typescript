d3009ac31b7187bf49f51ee6e4c1e7437fc5f902
Tech Luminaire <info@techluminaire.co.uk>
2020-05-09 23:43:09 +0100

    Got file picker upload queue working

--- src/main_api.ts
+++ src/main_api.ts
@@ -2,13 +2,12 @@ import store from './store/store';
 import AndroidImage from './models/data/android_image';
 
 interface ViewModelApi {
-  navigateTo(route: string): void;
-  uploadSharedFiles(androidImages: AndroidImage[]): Promise<void>;
-  uploadFileFromExternalPicker(androidImages: AndroidImage[]): Promise<void>;
   setUserAgent(userAgent: string): void;
-  uploadSharedFilesNoData(androidImages: AndroidImage[]): void;
-  uploadFileDetailsFromExternalPicker(androidImages: AndroidImage[]): Promise<void>;
-  injectBase64ImageData(androidImage: AndroidImage): void;
+  navigateTo(route: string): void;
+
+  uploadAndroidImageDetails(androidImages: AndroidImage[]): Promise<void>;
+  uploadAndroidImageData(androidImage: AndroidImage): void;
+  uploadAndroidSharedFiles(androidImages: AndroidImage[]): Promise<void>;
 }
 
 /// Defines external API to web app.  Allows native aps to interact with it
@@ -17,15 +16,12 @@ export default class MainApi implements ViewModelApi {
   public static Setup(viewModel: ViewModelApi) {
 
     const funcs = new MainApi();
-    viewModel.navigateTo = funcs.navigateTo;
-    viewModel.uploadSharedFiles = funcs.uploadSharedFiles;
     viewModel.setUserAgent = funcs.setUserAgent;
-    viewModel.uploadFileFromExternalPicker = funcs.uploadFileFromExternalPicker;
-    viewModel.uploadSharedFilesNoData = funcs.uploadSharedFilesNoData;
+    viewModel.navigateTo = funcs.navigateTo;
 
-    viewModel.uploadFileDetailsFromExternalPicker = funcs.uploadFileDetailsFromExternalPicker;
-    viewModel.injectBase64ImageData = funcs.injectBase64ImageData;
-
+    viewModel.uploadAndroidImageDetails = funcs.uploadAndroidImageDetails;
+    viewModel.uploadAndroidImageData = funcs.uploadAndroidImageData;
+    viewModel.uploadAndroidSharedFiles = funcs.uploadAndroidSharedFiles;
   }
 
   // Sets user agent, used by native apps
@@ -39,56 +35,15 @@ export default class MainApi implements ViewModelApi {
   }
 
   // Uploading files via android share, need to select gallery
-  public async uploadSharedFiles(androidImages: AndroidImage[]): Promise<void> {
+  public async uploadAndroidSharedFiles(androidImages: AndroidImage[]): Promise<void> {
     try {
       store.commit('updateLoading', true);
 
       this.navigateTo('/select_gallery/');
 
       const files = new Array<File>();
-      for (const androidImage of androidImages) {
-
-        const file = await androidImage.createFile(androidImage.base64Image);
-        files.push(file);
-      }
-
-      store.dispatch('setFilesToUpload', files);
-
-    } finally {
-      store.commit('updateLoading', false);
-    }
-  }
-
-  // Uploading files via android share, need to select gallery
-  public async uploadSharedFilesNoData(androidImages: AndroidImage[]): Promise<void> {
-    try {
-      store.commit('updateLoading', true);
-
-      this.navigateTo('/select_gallery/');
-
       store.dispatch('setAndroidImagesToUpload', androidImages);
 
-    } finally {
-      store.commit('updateLoading', false);
-    }
-  }
-
-  public async injectBase64ImageData(androidImage: AndroidImage) {
-    store.dispatch('setAndroidImagesToUpload', androidImage);
-  }
-
-  // Upload files, we should know the route already
-  public async uploadFileFromExternalPicker(androidImages: AndroidImage[]): Promise<void> {
-    try {
-      store.commit('updateLoading', true);
-
-      const files = new Array<File>();
-      for (const androidImage of androidImages) {
-
-        const file = await androidImage.createFile(androidImage.base64Image);
-        files.push(file);
-      }
-
       store.dispatch('setFilesToUpload', files);
 
     } finally {
@@ -96,9 +51,9 @@ export default class MainApi implements ViewModelApi {
     }
   }
 
-
-  public async uploadFileDetailsFromExternalPicker(androidImages: AndroidImage[]): Promise<void> {
-    window.console.log(`mainApi.uploadFileDetailsFromExternalPicker`);
+  // Uploads a list of filenames of images to be uploaded later one by one
+  public async uploadAndroidImageDetails(androidImages: AndroidImage[]): Promise<void> {
+    window.console.log(`mainApi.uploadAndroidImageDetails`);
     window.console.log(androidImages);
     try {
       store.commit('updateLoading', true);
@@ -108,5 +63,11 @@ export default class MainApi implements ViewModelApi {
     } finally {
       store.commit('updateLoading', false);
     }
+  }
+
+  // Uploads the image data for requested image
+  public async uploadAndroidImageData(androidImage: AndroidImage) {
+    window.console.log(`mainApi.uploadAndroidImageData`);
+    store.dispatch('injectImageData', androidImage);
   }
 }
--- src/models/data/android_image.ts
+++ src/models/data/android_image.ts
@@ -1,16 +1,8 @@
-export default class AndroidImage {
-    public base64Image: string = '';
-    public path: string = '';
-    public fileName: string = '';
-    public mimeType: string = '';
-    public file: File | null = null;
-
-    public async createFile(base64Image: string): Promise<File> {
-        const res = await fetch(base64Image);
-        const blob = await res.arrayBuffer();
-
-        const file = new File([blob], this.fileName, {type: this.mimeType});
-        this.file = file;
-        return file;
-    }
+export default interface AndroidImage {
+    index: number;
+    base64Image: string;
+    path: string;
+    fileName: string;
+    mimeType: string;
+    file: File;
 }
